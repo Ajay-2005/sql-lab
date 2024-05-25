4668528185e43680bbe681fefe1f8d80631475d2
Ajay A <aji478514@gmail.com>
2024-05-25 19:21:03 +0530

    Delete sales.sql

--- sales.sql
+++
@@ -1,3 +0,0 @@
-create table salesman(salesman_no int primary key,salesman_name varchar(15) not null,place varchar(20),phone int unique);
-
-create table sales_order(order_no int primary key,order_date date,salesman_no references salesman(salesman_no),)
